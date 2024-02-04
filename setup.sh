cd51271591574c0d5eedde702b6da4c7c899e7e6
Daniel V Mathew <danielvmathew27@gmail.com>
2024-02-04 17:34:21 +0530

    Added a setup script

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+Conf_Dir="${HOME}/.config"
+
+mkdir "${Conf_Dir}" > /dev/null 2>&1
+
+ln -s "${PWD}/git" "${Conf_Dir}" > /dev/null 2>&1
+ln -s "${PWD}/pandoc" "${Conf_Dir}" > /dev/null 2>&1
+ln -s "${PWD}/nvim" "${Conf_Dir}" > /dev/null 2>&1
+ln -s "${PWD}/starship/starship.toml" "${Conf_Dir}" > /dev/null 2>&1
+ln -s "${PWD}/tmux" "${Conf_Dir}" > /dev/null 2>&1
+ln -s "${PWD}/zsh/.zshrc" "${HOME}" > /dev/null 2>&1
+
+# Checking whether we are on Android or not
+# This is how pfetch checks for Android
+if [ -d /system/app ] && [ -d /system/priv-app ]; then
+    mkdir "${HOME}/.termux" > /dev/null 2>&1
+    ln -s "${PWD}/termux/colors.properties" "${HOME}/.termux" > /dev/null 2>&1
+    ln -s "${PWD}/termux/termux.properties" "${HOME}/.termux" > /dev/null 2>&1
+fi
