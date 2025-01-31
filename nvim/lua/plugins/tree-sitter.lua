a146738e50f92e7ed9aea29557540134a6ad4a9a
Daniel V Mathew <danielvmathew27@gmail.com>
2025-01-31 19:24:06 +0530

    nvim: added parser for openscad

--- nvim/lua/plugins/tree-sitter.lua
+++ nvim/lua/plugins/tree-sitter.lua
@@ -27,6 +27,20 @@ return {
                     branch = 'main',
                 },
             }
+
+            local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+            parser_config.openscad = {
+                install_info = {
+                    url = "~/projects/tree-sitter-openscad", -- local path or git repo
+                    files = {"src/parser.c"}, -- note that some parsers also require src/scanner.c or src/scanner.cc
+                    -- optional entries:
+                    branch = "master", -- default branch in case of git repo if different from master
+                    generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+                    requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+                },
+                filetype = "scad", -- if filetype does not match the parser name
+            }
+
         end
     }
 }
