85fd44e229d9bcc30908515a4e8ee4ea1107cc36
Daniel V Mathew <danielvmathew27@gmail.com>
2025-04-29 07:48:44 +0530

    nvim: cleaned true-zen.lua

--- nvim/lua/plugins/true-zen.lua
+++ nvim/lua/plugins/true-zen.lua
@@ -14,59 +14,59 @@ return {
                         -- background color
                         backdrop = 0,
                         minimum_writing_area = { -- minimum size of main window
-                        width = 90,
-                        height = 44,
+                            width = 90,
+                            height = 44,
+                        },
+                        quit_untoggles = true, -- type :q or :qa to quit Ataraxis mode
+                        padding = { -- padding windows
+                            left = 52,
+                            right = 52,
+                            top = 0,
+                            bottom = 0,
+                        },
+                        callbacks = { -- run functions when opening/closing Ataraxis mode
+                            open_pre = nil,
+                            open_pos = nil,
+                            close_pre = nil,
+                            close_pos = nil
+                        },
                     },
-                    quit_untoggles = true, -- type :q or :qa to quit Ataraxis mode
-                    padding = { -- padding windows
-                    left = 52,
-                    right = 52,
-                    top = 0,
-                    bottom = 0,
+                    minimalist = {
+                        -- save current options from any window except ones displaying these kinds of buffers
+                        ignored_buf_types = { "nofile" },
+                        -- options to be disabled when entering Minimalist mode
+                        options = {
+                            number = false,
+                            relativenumber = false,
+                            showtabline = 0,
+                            signcolumn = "no",
+                            statusline = "",
+                            cmdheight = 1,
+                            laststatus = 0,
+                            showcmd = false,
+                            showmode = false,
+                            ruler = false,
+                            numberwidth = 1
+                        },
+                        -- run functions when opening/closing Minimalist mode
+                        callbacks = {
+                            open_pre = nil,
+                            open_pos = nil,
+                            close_pre = nil,
+                            close_pos = nil
+                        },
+                    },
                 },
-                callbacks = { -- run functions when opening/closing Ataraxis mode
-                open_pre = nil,
-                open_pos = nil,
-                close_pre = nil,
-                close_pos = nil
-            },
-        },
-        minimalist = {
-            -- save current options from any window except ones displaying these kinds of buffers
-            ignored_buf_types = { "nofile" },
-            -- options to be disabled when entering Minimalist mode
-            options = {
-                number = false,
-                relativenumber = false,
-                showtabline = 0,
-                signcolumn = "no",
-                statusline = "",
-                cmdheight = 1,
-                laststatus = 0,
-                showcmd = false,
-                showmode = false,
-                ruler = false,
-                numberwidth = 1
-            },
-            -- run functions when opening/closing Minimalist mode
-            callbacks = {
-                open_pre = nil,
-                open_pos = nil,
-                close_pre = nil,
-                close_pos = nil
-            },
-        },
-    },
-    integrations = {
-        tmux = false, -- hide tmux status bar in (minimalist, ataraxis)
-        kitty = { -- increment font size in Kitty. Note: you must set `allow_remote_control socket-only` and `listen_on unix:/tmp/kitty` in your personal config (ataraxis)
-        enabled = false,
-        font = "+3"
-    },
-    twilight = false, -- enable twilight (ataraxis)
-    lualine = true -- hide nvim-lualine (ataraxis)
-},
-        }
-    end,
-}
+                integrations = {
+                    tmux = false, -- hide tmux status bar in (minimalist, ataraxis)
+                    kitty = { -- increment font size in Kitty. Note: you must set `allow_remote_control socket-only` and `listen_on unix:/tmp/kitty` in your personal config (ataraxis)
+                        enabled = false,
+                        font = "+3"
+                    },
+                    twilight = false, -- enable twilight (ataraxis)
+                    lualine = true -- hide nvim-lualine (ataraxis)
+                },
+            }
+        end,
+    }
 }
