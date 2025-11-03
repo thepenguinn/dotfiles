5673f15e93bc8fd3c311e3df28dfcf870767a38c
Daniel V Mathew <danielvmathew27@gmail.com>
2025-11-03 08:57:13 +0530

    zsh: added markdown-preview plugin

---
+++ nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && yarn install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+            vim.g.mkdp_browser = "markdown-previewer"
+        end,
+        ft = { "markdown" },
+    },
+}
